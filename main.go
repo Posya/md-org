cdeb92f1035d1f69de2e78757946c214809fb0d6
Pavel Kiselev <pavel.kiselev@gmail.com>
2017-12-20 17:34:25 +0300

    Added: testing, logging

--- main.go
+++ main.go
@@ -2,22 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
-type Action int
+type action int
+
+var trace, info *log.Logger
 
 const (
-	list Action = iota
+	list action = iota
 	check
 )
+
+func init() {
+	trace = log.New(os.Stdout, "Trace: ", log.Ldate|log.Ltime|log.Lshortfile)
+	info = log.New(os.Stdout, "Info: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 func main() {
 	fmt.Println("Hello, World!")
 }
 
-func do(act Action) error {
+func do(act action) error {
 	switch act {
 	case list:
+		trace.Println("This is list action")
+	case check:
+		trace.Println("This in check action")
 
 	}
+
+	return nil
 }
--- main_test.go
+++ main_test.go
@@ -3,5 +3,12 @@ package main
 import "testing"
 
 func TestMain(t *testing.T) {
-	main()
+	//main()
+}
+
+func TestDo(t *testing.T) {
+	err := do(list)
+	if err != nil {
+		t.Error("Error occurs: ", err)
+	}
 }
